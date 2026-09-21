f1459d88ecbe5b8fbe3e6c491d8b74afada67e35
Вадим Нестеров <v.nesterov@life-pay.ru>
2026-09-21 19:29:53 +0300

    refactor(disks): rename get_blocks to blocks

--- cmd/mcpd/main.go
+++ cmd/mcpd/main.go
@@ -184,7 +184,7 @@ func main() {
 			result, err = info.GetInfo(toolArgs)
 		} else if toolName == "load_average" {
 			result, err = load_average.GetLoadAverage(toolArgs)
-		} else if toolName == "get_blocks" {
+		} else if toolName == "blocks" {
 			result, err = blocks.GetBlocks(toolArgs)
 		} else if toolName == "get_os_release" {
 			result, err = os_release.GetOSRelease(toolArgs)
@@ -825,7 +825,7 @@ func processJSONRPC(session *Session, req JSONRPCRequest) {
 					},
 				},
 				map[string]interface{}{
-					"name": "get_blocks",
+					"name": "blocks",
 					"tools_group": "disks",
 					"description": "Lists block devices.",
 					"inputSchema": map[string]interface{}{
@@ -923,7 +923,7 @@ func processJSONRPC(session *Session, req JSONRPCRequest) {
 				// No need to spawn an isolated worker to read our own memory config
 				resultText, execErr = sudo_rules.GetSudoRules(params.Arguments, session.User, sudoConfig)
 
-			} else if params.Name == "list_files" || params.Name == "get_free" || params.Name == "get_usage" || params.Name == "list_processes" || params.Name == "delete_process" || params.Name == "get_interfaces" || params.Name == "list_connections" || params.Name == "nslookup" || params.Name == "curl" || params.Name == "arp" || params.Name == "ping" || params.Name == "memory_usage" || params.Name == "get_info" || params.Name == "load_average" || params.Name == "get_blocks" || params.Name == "get_os_release" {
+			} else if params.Name == "list_files" || params.Name == "get_free" || params.Name == "get_usage" || params.Name == "list_processes" || params.Name == "delete_process" || params.Name == "get_interfaces" || params.Name == "list_connections" || params.Name == "nslookup" || params.Name == "curl" || params.Name == "arp" || params.Name == "ping" || params.Name == "memory_usage" || params.Name == "get_info" || params.Name == "load_average" || params.Name == "blocks" || params.Name == "get_os_release" {
 				
 				// Standard privileged check payload
 				var baseArgs struct {
