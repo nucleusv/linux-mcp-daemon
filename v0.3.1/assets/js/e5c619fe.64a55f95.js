364beb23b94383a5e43f5ff5f3f65cbd9e572d29
Вадим Нестеров <v.nesterov@life-pay.ru>
2026-09-26 11:36:19 +0300

    Publish docs v0.3.2 and v0.3.1 (built once from their tags, with the version menu)

---
+++ v0.3.1/assets/js/e5c619fe.64a55f95.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_website=self.webpackChunkdocs_website||[]).push([["9416"],{5253(e){e.exports=JSON.parse('{"metadata":{"permalink":"/linux-mcp-daemon/v0.3.1/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ v0.3.2/assets/js/346b0fa1.f4e83998.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs_website=self.webpackChunkdocs_website||[]).push([["4999"],{9943(e){e.exports=JSON.parse('{"metadata":{"permalink":"/linux-mcp-daemon/v0.3.2/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":0,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
